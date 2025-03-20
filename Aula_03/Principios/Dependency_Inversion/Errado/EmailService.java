5c319ed2b36d27bb0d7b569b2897b1f3ddab3e47
Joselio Jr. <128568546+JoselioJr@users.noreply.github.com>
2025-03-20 00:34:25 -0300

    ✨feat: Adição dos relatórios.
    Adição dos relatórios de cada princípio.

--- Aula_03/Principios/Dependency_Inversion/Errado/EmailService.java
+++ Aula_03/Principios/Dependency_Inversion/Errado/EmailService.java
@@ -3,6 +3,6 @@ package Dependency_Inversion.Errado;
 public class EmailService {
     // Método de envio de E-amil
     public void enviarEmail(String mensagem){
-        System.out.println("Eciando E-mail: "+mensagem);
+        System.out.println("Eviando E-mail: "+mensagem);
     }
 }
