ecfc7f102abff71df13ff5af08bbf8f0751f7dfa
Joselio Jr. <128568546+JoselioJr@users.noreply.github.com>
2025-05-24 16:02:24 -0300

    🗑️remove: Removendo pasta desnecessária

--- Trabalho_Final/Padrao_Comportamental/Exemplo_Strategy/CartaoDeCredito.java
+++
@@ -1,8 +0,0 @@
-public class CartaoDeCredito implements PagamentoStrategy{
-
-    @Override
-    public void pagar(double valor) {
-        System.out.println("Pagamento no valor de R$"+valor+" realizado com cartão de crédito!");
-    }
-
-}
--- Trabalho_Final/Padrao_Comportamental/Exemplo_Strategy/Paypal.java
+++
@@ -1,8 +0,0 @@
-public class Paypal implements PagamentoStrategy{
-
-    @Override
-    public void pagar(double valor) {
-        System.out.println("Pagamento no valor de R$"+valor+" realizado com Paypal!");
-    }
-
-}
